53c80881a7c109cc9c10bab9c1a2d12820dee90c
Zou Lab <41780741+HuiyangYu@users.noreply.github.com>
2024-03-06 03:28:13 +0800

    Add files via upload

--- src/TGSFilter.cpp
+++ src/TGSFilter.cpp
@@ -7,7 +7,7 @@
 #include <thread>
 #include <algorithm>
 #include <unistd.h>
-#include <zlib.h>
+#include "zlib.h"
 #include "kseq.h"
 #include "DeflateOgzstream.cpp"
 
@@ -21,12 +21,13 @@ int  TGSFilter_usage() {
 		" Options:\n"
 		"   -i	<str>   input of fasta/q file\n"
 		"   -o	<str>   output of fasta/q file\n"
-		"   -q	<int>   min Phred average quality score [10]\n"
-		"   -l	<int>   min length of read [100]\n"
-		"   -s	<int>   Trim N nucleotides from the start of a read [0]\n"
-		"   -e	<int>   Trim N nucleotides from the end of a read [0]\n"
-		"   -t           number of threads [1]\n"
-		"   -h           show help [v1.05]\n"
+		"   -l	<int>   min length of read to out [1000]\n"
+		"   -q	<int>   min mean base quality [auto]\n"
+		"   -5	<int>   drop bases from the front (5') of a read [0]\n"
+		"   -3	<int>   drop bases from the tail (3') of a read [0]\n"
+		"   -w	<int>   windows szie to cut off low quality region [50]\n"
+		"   -t           number of threads [3]\n"
+		"   -h           show help [v1.06]\n"
 		"\n";
 	return 1;
 }
@@ -39,32 +40,32 @@ inline string add_Asuffix (string path) {
 	return path ;
 }
 
-int n_thread=1;
+int n_thread=3;
 int VECMAX =512000; //bp 
-int BATCH_SIZE = VECMAX;
-int BinWind = VECMAX;
+int BATCH_SIZE = 24;
+int BinWind = 24;
 
 class Para_A24 {
 	public:
-		int minQ;
 		int ReadLength;
 		int MinLength;
+		int AverQ;
 		int HeadCrop;
 		int TailCrop;
-		int AverQ;
+		int Window;
 		int Infq;
 		int Outfq;
 		bool OUTGZ;
 		string InFile;
 		string OutFile;
 
-		Para_A24() {	
-			minQ=10;
+		Para_A24() {
 			ReadLength=0;
-			MinLength=100;
+			MinLength=1000;
+			AverQ=-1;
 			HeadCrop=0;
 			TailCrop=0;
-			AverQ=0;
+			Window=50;
 			Infq=2;
 			Outfq=2;
 			OUTGZ=false;
@@ -117,25 +118,33 @@ int TGSFilter_cmd(int argc, char **argv, Para_A24 * P2In) {
 		else if (flag == "q") {
 			if(i + 1 == argc) {LogLackArg(flag); return 1;}
 			i++;
-			P2In->minQ=atoi(argv[i]);
+			P2In->AverQ=atoi(argv[i]);
 		}
 		else if (flag == "l") {
 			if(i + 1 == argc) {LogLackArg(flag); return 1;}
 			i++;
 			P2In->MinLength=atoi(argv[i]);
+			if (P2In->MinLength<1){
+				P2In->MinLength=1;
+			}
 		}
 
 		//Trim header and reads options
-		else if (flag == "s") {
+		else if (flag == "5") {
 			if(i + 1 == argc) {LogLackArg(flag); return 1;}
 			i++;
 			P2In->HeadCrop=atoi(argv[i]);
 		}
-		else if (flag == "e"){
+		else if (flag == "3"){
 			if(i + 1 == argc) {LogLackArg(flag); return 1;}
 			i++;
 			P2In->TailCrop=atoi(argv[i]);
-		}
+		} 
+		else if (flag == "w"){
+			if(i + 1 == argc) {LogLackArg(flag); return 1;}
+			i++;
+			P2In->Window=atoi(argv[i]);
+		} 
 		//Other options
 		else if (flag  ==  "t") {
 			if(i + 1 == argc) {LogLackArg(flag) ; return 1;}
@@ -207,7 +216,7 @@ int Get_qType(Para_A24 * P2In){
 
 	gzFile fp;
   	kseq_t *seq;
-  	int len;
+  	uint32_t len;
 
 	fp = gzopen((P2In->InFile).c_str(), "r");
   	seq = kseq_init(fp);
@@ -216,13 +225,25 @@ int Get_qType(Para_A24 * P2In){
 	int maxSeq=5000;
 	int minQ=50000;
 	int maxQ=0;
+	uint64_t sumQ = 0;
+	uint64_t sumLen = 0;
 	int Lengths[maxSeq];
 	string qual;
-	for (int A=0 ; A<(maxSeq) && ((len = kseq_read(seq)) >= 0); A++){
-		Lengths[seqNum]=(seq->seq.l);
+	for (int A=0 ; ((len = kseq_read(seq)) >= 0); A++){
+		if (seqNum>=maxSeq){
+			break;
+		}
+
+		int length=seq->seq.l;
+		if (length<10000){
+			continue;
+		}
+		Lengths[seqNum]=length;
 		seqNum++;
 		qual=seq->qual.s;
-		for (int i=0; i<(seq->qual.l); i++) {
+		sumLen+=length;
+		for (int i=0; i<length; i++) {
+			sumQ+=qual[i];
 			if(minQ>qual[i]) {
 				minQ=qual[i];
 			}
@@ -237,13 +258,14 @@ int Get_qType(Para_A24 * P2In){
 	sort(Lengths, Lengths + seqNum);
 	int middleIndex = seqNum / 2;
 	P2In->ReadLength = Lengths[middleIndex];
+	//cout<<"INFO: middle read length: "<<(P2In->ReadLength)<<" bp"<<endl;
 
 	int qType=0;
 	if (maxQ>0){
-		if(minQ >= 33 &&  minQ <= 78  &&  maxQ >= 33 && maxQ <= 78) {
+		if(minQ >= 33 &&  minQ <= 78  &&  maxQ >= 33 && maxQ <= 127) {
 			qType=33;
 		}
-		else if (minQ >= 64  &&  minQ <= 108  &&  maxQ >= 64 && maxQ <= 108){
+		else if (minQ >= 64  &&  minQ <= 108  &&  maxQ >= 64 && maxQ <= 127){
 			qType=64;
 		}
 		else if (minQ < 55) {
@@ -252,22 +274,36 @@ int Get_qType(Para_A24 * P2In){
 		else {
 			qType=64;
 		}
-		P2In->AverQ=(P2In->AverQ)+qType;
 	}
+	int meanQ=int(sumQ/sumLen)-qType;
+	//cout <<"sumQ: "<<sumQ<<" sumLen: "<<sumLen<<endl;
+	//cout<<"INFO: mean base quality: "<<meanQ<<endl;
+	if(P2In->AverQ<0){
+		if (meanQ>=25){
+			P2In->AverQ=20;
+		}else if(meanQ>=15){
+			P2In->AverQ=10;
+		}else {
+			P2In->AverQ=7;
+		}	
+	}
+	cout<<"INFO: min mean base quality was set to: "<<(P2In->AverQ)<<endl;
+	//cout << "INFO: Qtype "<<qType<<endl;
+	P2In->AverQ=(P2In->AverQ)+qType;
 
-	return maxQ;
+	return 0;
 }
 
-int  CalcAvgQuality(const string& qual) {
-	int n = qual.length();
+double CalcAvgQuality(const string &qual) {
+	long long int  n = qual.length();
 	if (n == 0) {
 		return 0;
 	}
-	long long sumQ = 0;
-	for (int i = 0; i < n; i++) {
+	long long int sumQ = 0;
+	for (long long int i = 0; i < n; i++) {
 		sumQ += qual[i];
 	}
-	return  int (sumQ) / n;
+	return  (sumQ) / n;
 }
 
 void Filter_fastq_reads(Para_A24 * P2In, string &OUT_DATA, int Start, int End, 
@@ -280,14 +316,56 @@ void Filter_fastq_reads(Para_A24 * P2In, string &OUT_DATA, int Start, int End,
 			int seqLen = SEQ[i].length();
 			int qualLen = QUAL[i].length();
 			if ((totalCrop < seqLen) && (seqLen == qualLen)) {
-				SEQ[i] = SEQ[i].substr(headCrop, seqLen - totalCrop);
-				QUAL[i] = QUAL[i].substr(headCrop, qualLen - totalCrop);
-				seqLen = SEQ[i].length();
+				string seq = SEQ[i].substr(headCrop, seqLen - totalCrop);
+				string qual = QUAL[i].substr(headCrop, qualLen - totalCrop);
+				seqLen = seq.length();
 				if (seqLen >= (P2In->MinLength)) {
-					double avgQuality = CalcAvgQuality(QUAL[i]);
-					if (avgQuality >= (P2In->AverQ)) {
-						OUT_DATA=OUT_DATA+"@"+ID[i]+"\n" + SEQ[i] + "\n+\n" + QUAL[i] + "\n";
+					string out_seq;
+					string out_qual;
+					string segment_seq;
+					string segment_qual;
+					double avgQuality;
+					string seq_id=ID[i];
+					int window=P2In->Window;
+					int start_idx = 0;
+					int end_idx = window;
+					int pass_num=0;
+					while (end_idx < seqLen) {
+        				segment_seq = seq.substr(start_idx, window);
+        				segment_qual = qual.substr(start_idx, window);
+						avgQuality = CalcAvgQuality(segment_qual);
+						if (avgQuality >= (P2In->AverQ)) {
+							out_seq+=segment_seq;
+							out_qual+=segment_qual;
+						}else{
+							if (out_seq.length()>=(P2In->MinLength)){
+								pass_num++;
+								if (pass_num>=2){
+									seq_id=ID[i]+":"+std::to_string(pass_num);
+								}
+								OUT_DATA=OUT_DATA+"@"+seq_id+"\n" + out_seq + 
+										 "\n+\n" + out_qual + "\n";
+								out_seq="";
+								out_qual="";
+							}
+						}
+						start_idx+=window;
+						end_idx=start_idx+window;
 					}
+					//Processing the final segment
+					segment_seq = seq.substr(start_idx);
+					segment_qual = qual.substr(start_idx);
+					avgQuality = CalcAvgQuality(segment_qual);
+					if (avgQuality >= (P2In->AverQ)){
+						out_seq+=segment_seq;
+						out_qual+=segment_qual;
+					}
+					pass_num++;
+					if (pass_num>=2){
+						seq_id=ID[i]+":"+std::to_string(pass_num);
+					}
+					OUT_DATA=OUT_DATA+"@"+seq_id+"\n" + out_seq + 
+								"\n+\n" + out_qual + "\n";
 				}
 			}
 		}
@@ -296,14 +374,50 @@ void Filter_fastq_reads(Para_A24 * P2In, string &OUT_DATA, int Start, int End,
 			int seqLen = SEQ[i].length();
 			int qualLen = QUAL[i].length();
 			if ((totalCrop < seqLen) && (seqLen == qualLen)) {
-				SEQ[i] = SEQ[i].substr(headCrop, seqLen - totalCrop);
-				QUAL[i] = QUAL[i].substr(headCrop, qualLen - totalCrop);
-				seqLen = SEQ[i].length();
+				string seq = SEQ[i].substr(headCrop, seqLen - totalCrop);
+				string qual = QUAL[i].substr(headCrop, qualLen - totalCrop);
+				seqLen = seq.length();
 				if (seqLen >= (P2In->MinLength)) {
-					double avgQuality = CalcAvgQuality(QUAL[i]);
-					if (avgQuality >= (P2In->AverQ)) {
-						OUT_DATA=OUT_DATA+">"+ID[i]+"\n" + SEQ[i] + "\n";
+					string out_seq;
+					string segment_seq;
+					string segment_qual;
+					double avgQuality;
+					string seq_id=ID[i];
+					int window=P2In->Window;
+					int start_idx = 0;
+					int end_idx = window;
+					int pass_num=0;
+					while (end_idx < seqLen) {
+        				segment_seq = seq.substr(start_idx, window);
+        				segment_qual = qual.substr(start_idx, window);
+						avgQuality = CalcAvgQuality(segment_qual);
+						if (avgQuality >= (P2In->AverQ)) {
+							out_seq+=segment_seq;
+						}else{
+							if (out_seq.length()>=(P2In->MinLength)){
+								pass_num++;
+								if (pass_num>=2){
+									seq_id=ID[i]+":"+std::to_string(pass_num);
+								}
+								OUT_DATA=OUT_DATA+"@"+seq_id+"\n" + out_seq + "\n";
+								out_seq="";
+							}
+						}
+						start_idx+=window;
+						end_idx=start_idx+window;
 					}
+					//Processing the final segment
+					segment_seq = seq.substr(start_idx);
+					segment_qual = qual.substr(start_idx);
+					avgQuality = CalcAvgQuality(segment_qual);
+					if (avgQuality >= (P2In->AverQ)){
+						out_seq+=segment_seq;
+					}
+					pass_num++;
+					if (pass_num>=2){
+						seq_id=ID[i]+":"+std::to_string(pass_num);
+					}
+					OUT_DATA=OUT_DATA+"@"+seq_id+"\n" + out_seq + "\n";
 				}
 			}
 		}
@@ -318,10 +432,10 @@ void Filter_fasta_reads(Para_A24 * P2In,string &OUT_DATA, int Start, int End,
 	for (int i = Start; i < End; i++) {
 		int seqLen = SEQ[i].length();
 		if (totalCrop < seqLen) {
-			SEQ[i] = SEQ[i].substr(headCrop, seqLen - totalCrop);
-			seqLen = SEQ[i].length();
+			string seq = SEQ[i].substr(headCrop, seqLen - totalCrop);
+			seqLen = seq.length();
 			if (seqLen >= (P2In->MinLength)) {
-				OUT_DATA=OUT_DATA+">"+ID[i]+"\n" + SEQ[i] + "\n";
+				OUT_DATA=OUT_DATA+">"+ID[i]+"\n" + seq + "\n";
 			}
 		}
 	}
@@ -357,8 +471,6 @@ void Filter_reads_gz(Para_A24 * P2In, int & Start, int & End,
 		GZData.compressData(OUT_DATA.c_str(), inputSize, ComPresseData, 
 							CompressedSize,OUT_BUFFER_SIZE,thread_id);
 	}
-	
-
 }
 
 int Run_seq_filter (Para_A24 * P2In) {
@@ -586,12 +698,6 @@ int Run_TGSFilter(Para_A24 * P2In) {
 
 	Get_qType(P2In);
 	int read_length=(P2In->ReadLength);
-	cout<<"INFO: middle read length is "<<read_length<<" bp"<<endl;
-	
-	BinWind=int(VECMAX/read_length);
-	if (BinWind<1){
-		BinWind=1;
-	}
 	BATCH_SIZE = BinWind*n_thread;
 
 	Run_seq_filter(P2In);
