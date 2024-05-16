02287698f6a2db02b32dce958f00b0e433a0b949
Zou Lab <41780741+HuiyangYu@users.noreply.github.com>
2024-05-16 22:52:40 +0800

    Add files via upload

--- include/report.h
+++ include/report.h
@@ -37,19 +37,7 @@ struct Selection
     vector<string> ids;
 };
 
-struct OnePlotData
-{
-    LenDisData downLenDis;
-    QualDisData downQualDis;
-    LinePlotData downReadsQual;
-    LinePlotData down5pReadsQual;
-    LinePlotData down3pReadsQual;
-    LinePlotData downBasesContents;
-    LinePlotData down5pBasesContents;
-    LinePlotData down3pBasesContents;
-};
-
-struct TwoPlotData
+struct PlotData
 {
     LenDisData rawLenDis;
     LenDisData cleanLenDis;
@@ -69,30 +57,3 @@ struct TwoPlotData
     LinePlotData clean3pBasesContents;
 };
 
-struct ThreePlotData
-{
-    LenDisData downLenDis;
-    LenDisData cleanLenDis;
-    LenDisData rawLenDis;
-    QualDisData downQualDis;
-    QualDisData rawQualDis;
-    QualDisData cleanQualDis;
-    LinePlotData downReadsQual;
-    LinePlotData down5pReadsQual;
-    LinePlotData down3pReadsQual;
-    LinePlotData downBasesContents;
-    LinePlotData down5pBasesContents;
-    LinePlotData down3pBasesContents;
-    LinePlotData rawReadsQual;
-    LinePlotData cleanReadsQual;
-    LinePlotData raw5pReadsQual;
-    LinePlotData clean5pReadsQual;
-    LinePlotData raw3pReadsQual;
-    LinePlotData clean3pReadsQual;
-    LinePlotData rawBasesContents;
-    LinePlotData cleanBasesContents;
-    LinePlotData raw5pBasesContents;
-    LinePlotData clean5pBasesContents;
-    LinePlotData raw3pBasesContents;
-    LinePlotData clean3pBasesContents;
-};
